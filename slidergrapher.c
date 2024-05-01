111ac872c670b87e239fe87ab4663ce9a07b08d2
Kovacs Lorant <kovacslorantjanos@gmail.com>
2024-05-01 16:12:20 +0200

    slidergrapher with parameter

--- slidergrapher.c
+++ slidergrapher.c
@@ -20,15 +20,20 @@ static void draw_function(GtkDrawingArea *area, cairo_t *cr, int width,
   cairo_fill(cr);
 }
 
+static gchar* txtfft_file = NULL;
+static GOptionEntry slidergrapher_option_entries[] =
+{
+  { "txtfft_file", 'f', 0, G_OPTION_ARG_STRING, &txtfft_file, "Name of the input file.", "<txtfft file>"},
+  G_OPTION_ENTRY_NULL
+};
+
 static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Hello");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-
 
   GtkWidget *area = gtk_drawing_area_new();
-  gtk_drawing_area_set_content_width(GTK_DRAWING_AREA(area), 100);
-  gtk_drawing_area_set_content_height(GTK_DRAWING_AREA(area), 100);
+  gtk_drawing_area_set_content_width(GTK_DRAWING_AREA(area), 1920);
+  gtk_drawing_area_set_content_height(GTK_DRAWING_AREA(area), 1200);
   gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(area), draw_function, NULL,
                                  NULL);
 
@@ -45,10 +50,16 @@ static void activate(GtkApplication *app, gpointer user_data) {
 }
 
 int main(int argc, char **argv) {
+  GOptionGroup* options = g_option_group_new("slidergrapher", "slidergrapher options:", "Show slidergrapher options", NULL, NULL);
+  g_option_group_add_entries(options, slidergrapher_option_entries);
+
   GtkApplication *app;
   int status;
 
   app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+
+  g_application_add_option_group(G_APPLICATION(app), options);
+
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
