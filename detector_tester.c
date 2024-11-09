c2d403f3fa8ee2690bc986163543f2f5e05a8700
Kovacs Lorant <kovacslorantjanos@gmail.com>
2024-11-09 16:43:53 +0100

    detector_tester calls speedsites

--- detector_tester.c
+++ detector_tester.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "detectors/speeddetector/speedsites.h"
 #include "frames.h"
@@ -17,4 +18,19 @@ int main (int argc, char* argv[]){
   frames_load_from_file(&frames, input_file);
 
   fprintf(stderr, "data_len: %d\n", frames.data_len);
+
+  FastChangingDetectorInput detector_input;
+  detector_input.n = 4;
+  detector_input.g = 0.1;
+  detector_input.frames = &frames;
+  detector_input.frames_begin = 50;
+  detector_input.frames_end = frames.data_len;
+
+  int *detector_out = (int*)malloc(frames.frame_len * sizeof(int));
+  detect_fast_changing(detector_out, &detector_input);
+
+  for (int i = 0; i < frames.frame_len; ++i) {
+    printf("%d", detector_out[i]);
+  }  
+  printf("\n");
 }
